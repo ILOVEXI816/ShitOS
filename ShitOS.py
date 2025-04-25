cd0b7edbe888e3d7b8c12ddd5b468b356741a227
I_LOVE_XI816 <homyarek@gmail.com>
2025-04-25 19:42:23 +0400

    ShitOS 1.1

--- ShitOS.py
+++ ShitOS.py
@@ -1,20 +1,21 @@
 import time
-print("welcome to ShitOS v1.2.0!")
+
+print("welcome to ShitOS v1.0!")
 print("loading...    ")
-print("rom = 5mb")
-print("ram = 90mb")
 print("please wait...")
-print("full memory= 95mb")
+time.sleep(5)
+folders=["system", "programs", "files"]
 while True:
  command1 = input("print command: ")
  if command1 == "info" : 
   print(" ...........")
-  print(" ShitOS v.1.2.0")
+  print(" ShitOS v.1.0")
+  print("ShitRTX 4090ti")
+  print("Shintel core i5-6500")
  if command1 == "data" : 
    print(" ...........")
-   print("folder: system")
-   print("folder: programs")
-   print("folder: files")
+   for i in folders:
+       print(f"folder: {i}")
    itm = input("select: ")
    if itm == "system" :
      print(" ...........")
@@ -22,7 +23,7 @@ while True:
      print("folder: nothing")
      sysdat = input("select: ")
      if sysdat == "code" :
-      print("haha nothing!")
+      print("Shit shit shit shit")
    if itm == "programs" :
     print(" ...........")
     print("pys: calculator")
@@ -31,15 +32,39 @@ while True:
     promdat = input("select: ")
     if promdat == "calculator" :
      print(" ...........")
-     a = float(input("one: "))
-     b = float(input("two: "))
-     c = a + b
-     print(c)
-    if promdat == "time" :
+     a = input("one: ")
+     b = input("two: ")
+     try:
+         a = float(a)
+         b = float(b)
+         print(a + b)
+     except:
+         print("Enter valid numbers!")
+    elif promdat == "time" :
      print(" ...........")
      t = time.localtime(time.time())
-     localtime = time.asctime(t)
-     str = "Current Time:" + time.asctime(t)
- 
-     print(str);
+     print("Current time:", time.asctime(t));
+    elif promdat == "word compilator":
+        print("Notepad-- v.2.1.3. 'exit' to exit.")
+        ext = ""
+        file = ""
+        while ext != "exit":
+            ext = input("~ ")
+            file += f"{ext}\n"
+        print("OK.")
+        print("[r]ead, [s]ave, [p]rint, [i]gnore")
+        var = input("what to do with this file? ")
+        if var == "r" or var == "i":
+            pass
+        elif var == "s":
+            with open(file.split('\n')[0], "w") as f:
+                f.write(file)
+        elif var == "p":
+            print(f"{file}")
+        else:
+            pass
+ elif command1 == "mkdir":
+     folders.append(command[6:])
+ elif command1 == "exit":
+     exit()
     
